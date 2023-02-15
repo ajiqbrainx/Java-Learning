490e50b79486b0ba4a288997ef18c0987fe28b5c
Ajith.j <ajith.j@qbrainx.com>
2023-02-15 12:27:22 +0530

    New

---
+++ src/Practice/LastThreeChart.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class LastThreeChart {
+    public static void main(String[] args) {
+        String name = "ajithashok2530@gmail.comm";
+        String check = name.substring(name.length() - 10);
+        if (check.equals("@gmail.com")) System.out.println("It's match");
+        else System.out.println("It's not match");
+    }
+}
