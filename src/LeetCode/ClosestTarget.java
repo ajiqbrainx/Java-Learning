e34a46d8090d8174d8dd5cac1ebdc4f2913cdee7
Ajith.j <ajith.j@qbrainx.com>
2023-10-16 17:38:07 +0530

    16/10/2023

---
+++ src/LeetCode/ClosestTarget.java
@@ -0,0 +1,78 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class ClosestTarget {
+    public static void main(String[] args) {
+
+        int[] nums = {4,0,5,-5,3,3,0,-4,-5};
+        int target = -2;
+
+        int[] sortedArray = new int[nums.length];
+
+        System.arraycopy(nums, 0, sortedArray, 0, nums.length);
+
+        int output = 0;
+        int n = sortedArray.length;
+        int temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (sortedArray[j] > sortedArray[j + 1]) {
+                    temp = sortedArray[j];
+                    sortedArray[j] = sortedArray[j + 1];
+                    sortedArray[j + 1] = temp;
+                }
+            }
+        }
+        boolean targetIsPresent = false;
+
+
+        int firstElement = sortedArray[0];
+        int lastElement = sortedArray[nums.length - 1];
+
+        if (firstElement == target || lastElement == target) {
+            for (int i = 0; i < 3; i++) {
+                output += sortedArray[i];
+            }
+            System.out.println(output);
+        } else {
+            for (int i = 0; i < sortedArray.length; i++) {
+                if (target == sortedArray[i]) {
+                    int first = sortedArray[i - 1];
+                    int last = sortedArray[i + 1];
+                    output = first + target + last;
+                    targetIsPresent = true;
+                }
+
+            }
+        }
+
+
+        if (!targetIsPresent) {
+
+            if (firstElement > target) {
+                for (int i = 0; i < 3; i++) {
+                    output += sortedArray[i];
+                }
+                System.out.println(output);
+            } else if (lastElement < target) {
+                int x = sortedArray.length;
+                if (x >= 3) {
+                    for (int i = x - 3; i < x; i++) {
+                        output += sortedArray[i];
+                    }
+                }
+                System.out.println(output);
+
+            }
+            else{
+                for (int i = 0; i < 3; i++) {
+                    output += sortedArray[i];
+                }
+//                output=target;
+            }
+
+        }
+        System.out.println(output);
+    }
+}
---
+++ src/LeetCode/PhoneNumberString.java
@@ -0,0 +1,68 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhoneNumberString {
+    public static void main(String[] args) {
+        String digits = "23";
+
+        Map<Integer, List<String>> valueLists = new HashMap<>();
+        valueLists.put(2, new ArrayList<>());
+        valueLists.put(3, new ArrayList<>());
+        valueLists.put(4, new ArrayList<>());
+        valueLists.put(5, new ArrayList<>());
+        valueLists.put(6, new ArrayList<>());
+        valueLists.put(7, new ArrayList<>());
+        valueLists.put(8, new ArrayList<>());
+        valueLists.put(9, new ArrayList<>());
+
+        valueLists.get(2).add("a");
+        valueLists.get(2).add("b");
+        valueLists.get(2).add("c");
+        valueLists.get(3).add("d");
+        valueLists.get(3).add("e");
+        valueLists.get(3).add("f");
+        valueLists.get(4).add("g");
+        valueLists.get(4).add("h");
+        valueLists.get(4).add("i");
+        valueLists.get(5).add("j");
+        valueLists.get(5).add("k");
+        valueLists.get(5).add("l");
+        valueLists.get(6).add("m");
+        valueLists.get(6).add("n");
+        valueLists.get(6).add("o");
+        valueLists.get(7).add("p");
+        valueLists.get(7).add("q");
+        valueLists.get(7).add("r");
+        valueLists.get(7).add("s");
+        valueLists.get(8).add("t");
+        valueLists.get(8).add("u");
+        valueLists.get(8).add("v");
+        valueLists.get(9).add("w");
+        valueLists.get(9).add("x");
+        valueLists.get(9).add("y");
+        valueLists.get(9).add("z");
+
+        List<String> output = new ArrayList<>();
+
+        for (int k = 0; k < digits.length(); k++) {
+            char c = digits.charAt(k);
+            char c1 = digits.charAt(k + 1);
+
+
+            List<String> value1 = valueLists.get(Character.getNumericValue(c));
+            List<String> value2 = valueLists.get(Character.getNumericValue(c1));
+            for (int i = 0; i < value1.size(); i++) {
+                for (int j = 0; j < value2.size(); j++) {
+                    output.add(value1.get(i)+value2.get(j));
+                }
+            }
+            break;
+        }
+        System.out.println(output);
+
+    }
+}
