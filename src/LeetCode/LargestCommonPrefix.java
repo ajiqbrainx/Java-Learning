ec5c96f60759b05f746ae1459bb3fd60ad060409
Ajith.j <ajith.j@qbrainx.com>
2023-10-16 10:43:27 +0530

    16/10/2023

--- src/LeetCode/LargestCommonPrefix.java
+++ src/LeetCode/LargestCommonPrefix.java
@@ -4,13 +4,36 @@ import java.util.Arrays;
 
 public class LargestCommonPrefix {
     public static void main(String[] args) {
-        String[] strs = {"abcd", "abicd", "acds"};
-        Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[strs.length-1];
+        String[] strs = {"flower","flower","flower","flower"};
 
-        System.out.println(first);
-        System.out.println(last);
+        Character dummy = null;
 
+        StringBuffer stringBuffer = new StringBuffer();
+
+
+        int index = 0;
+        outerLoop:
+        for (int j = 0; j ==index; j++) {
+            for (int i = 0; i < strs.length; i++) {
+                if (strs[i].isEmpty() || index >= strs[i].length() ) {
+                    break outerLoop;
+                } else if (i != 0 && index < strs[i].length()) {
+                    if (dummy == strs[i].charAt(index)) {
+                    } else {
+                        break outerLoop;
+                    }
+                } else if (i == 0) {
+                    if (index < strs[i].length()) {
+                        dummy = strs[i].charAt(index);
+                    }
+                }
+            }
+            stringBuffer.append(dummy);
+            dummy = null;
+            index++;
+        }
+        String output = stringBuffer.toString();
+
+        System.out.println(output);
     }
 }
