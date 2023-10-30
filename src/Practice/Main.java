f501d0f642d891351ab48c3db3eeecbfafa14e6d
Ajith.j <ajith.j@qbrainx.com>
2023-10-30 09:41:04 +0530

    26/10/2023

---
+++ src/Practice/Main.java
@@ -0,0 +1,17 @@
+package Practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Mains {
+    public static void main(String[] args) {
+        List<Boolean> list = new ArrayList<>();
+        list.add(true);
+//        list.add(Boolean.parseBoolean("FalSe"));
+        list.add(Boolean.parseBoolean("23"));
+        list.add(Boolean.TRUE);
+        System.out.print(list.size());
+        System.out.print(list.get(1) instanceof Boolean);
+    }
+
+}
