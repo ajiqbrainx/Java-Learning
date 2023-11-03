9f69be4c2e0f23aa226936f2eec6d9d0db1f08ef
Ajith.j <ajith.j@qbrainx.com>
2023-11-03 17:51:44 +0530

    3/11/2023

---
+++ src/Practice/Fib.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class Fib {
+    public static void main(String[] args) {
+        int first = 0;
+        int last = 1;
+        int input = 3;
+        int output;
+
+        System.out.println(first);
+        System.out.println(last);
+        for (int i = 2; i < input; i++) {
+            output = first + last;
+            System.out.println(output);
+            first = last;
+            last = output;
+
+        }
+
+    }
+}
---
+++ src/Practice/StringNew.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class StringNew {
+    public static void main(String[] args) {
+
+        String a = "ajith";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter string ");
+        String name = scanner.next();
+        System.out.println(a.equals(name));
+    }
+}
