4708f66a49d65832b4d6fea59385b7af6bd923de
Ajith.j <ajith.j@qbrainx.com>
2023-11-06 10:35:36 +0530

    6/11/2023

--- src/Interview/Swap.java
+++ src/Interview/Swap.java
@@ -2,13 +2,17 @@ package Interview;
 
 public class Swap {
     public static void main(String[] args) {
-        int a = 99;
-        int b = 29;
+//        int a = 10;
+//        int b = 20;
+//
+//        a = a + b; //128
+//        b = a - b; //99
+//        a = a - b; //29
+//        System.out.println(a);
+//        System.out.println(a + '-' + "ajith" + b+a);
+        int x = 15;
+        int result = (x < 6) ? 7 : (x > 24) ? 8 : 9;
 
-        a = a + b; //128
-        b = a - b; //99
-        a = a - b; //29
-        System.out.println(a);
-        System.out.println(b);
+        System.out.println(result);
     }
 }
