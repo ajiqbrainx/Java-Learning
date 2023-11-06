4ef8527e92bfdfb421f614d6b2135bdab6e99861
Ajith.j <ajith.j@qbrainx.com>
2023-11-06 09:45:23 +0530

    3/11/2023

--- src/Interview/Swap.java
+++ src/Interview/Swap.java
@@ -2,12 +2,12 @@ package Interview;
 
 public class Swap {
     public static void main(String[] args) {
-        int a=99;
-        int b=29;
+        int a = 99;
+        int b = 29;
 
-        a=a+b; //128
-        b=a-b; //99
-        a=a-b; //29
+        a = a + b; //128
+        b = a - b; //99
+        a = a - b; //29
         System.out.println(a);
         System.out.println(b);
     }
