96ac691c4a32ab67ea55e1aa80d296f782c8fc3f
Ajith.j <ajith.j@qbrainx.com>
2024-01-04 09:45:42 +0530

    09/12/2023

--- src/LeetCode/FindIndex.java
+++ src/LeetCode/FindIndex.java
@@ -1,33 +1,37 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 public class FindIndex {
     public static void main(String[] args) {
-        int[] nums = {1,3,5,6};
-        int target = 7;
-        int output = 0;
-        boolean targetIsPresent = false;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (target == nums[i]) {
-                output = i;
-                targetIsPresent = true;
-                break;
-            }
-        }
-
-        if (targetIsPresent) {
-            System.out.println(output);
-        } else {
-            System.out.println("It is not there");
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] < target) {
-                    output = i + 1;
-                } else {
-                    break;
-                }
-            }
-            System.out.println(output);
-        }
+        int[] nums = {7,8,9};
+        int [] y={7,7,7};
+        System.out.println(Arrays.compare(nums,y)+" "+Arrays.mismatch(nums,y));
+//        int target = 2;
+//        int output = 0;
+//        boolean targetIsPresent = false;
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            if (target == nums[i]) {
+//                output = i;
+//                targetIsPresent = true;
+//                break;
+//            }
+//        }
+//
+//        if (targetIsPresent) {
+//            System.out.println(output);
+//        } else {
+//            System.out.println("It is not there");
+//            for (int i = 0; i < nums.length; i++) {
+//                if (nums[i] < target) {
+//                    output = i + 1;
+//                } else {
+//                    break;
+//                }
+//            }
+//            System.out.println(output);
+//        }
 
     }
 }
