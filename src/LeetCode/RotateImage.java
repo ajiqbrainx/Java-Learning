bc5b2b3af7d08a97f57c483b1f5ff021458ba868
Ajith.j <ajith.j@qbrainx.com>
2024-01-29 18:02:16 +0530

    29/01/2024

--- src/LeetCode/RotateImage.java
+++ src/LeetCode/RotateImage.java
@@ -1,30 +1,35 @@
-//package LeetCode;
-//
-//import java.awt.*;
-//
-//public class RotateImage {
-//
-//
-//    public static void main(String[] args) {
-//        int[][] matrix = {{1, 2, 3}, {4, 5, 6,}, {7, 8, 9}};
-//        int length = matrix.length - 1;
-//        int[][] output = new int[length - 1][length - 1];
-//        int index = 0;
-//        finalMatrix(matrix, length, output, index);
-//
-//
-//    }
-//
-//    static int[][] finalMatrix(int[][] matrix, int length, int[][] output, int index) {
-//
-//        if (index <= 0 || length >= 0) {
-//            output[length][index] = matrix[length][index];
-//            finalMatrix(matrix, length, output, index);
-//        } else ()
-//
-//
-//        return output;
-//    }
-//
-//
-//}
+package LeetCode;
+
+import java.awt.*;
+import java.util.Arrays;
+
+public class RotateImage {
+
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6,}, {7, 8, 9}};
+        int length = matrix.length;
+        int[][] output = new int[length][length];
+
+        matrix =output;
+
+        int row = 0;
+        int column = 0;
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = length - 1; j >= 0; j--) {
+                int value = matrix[j][i];
+                output[row][column] = value;
+                column++;
+            }
+            column = 0;
+            row++;
+        }
+        matrix =output;
+        System.out.println(Arrays.toString(matrix));
+
+    }
+
+
+}
