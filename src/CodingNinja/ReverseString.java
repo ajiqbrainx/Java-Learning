7eda9715906fedd566956674f3b4c977c74f6255
Ajith <ajithashok2530@gmail.com>
2024-02-03 19:47:17 +0530

    03/02/24

---
+++ src/CodingNinja/ReverseString.java
@@ -0,0 +1,20 @@
+package CodingNinja;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String input = "10";
+        var output = reverseString(input);
+        System.out.println(output);
+
+    }
+
+    public static String reverseString(String str) {
+        String output = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            output = output + str.charAt(i);
+        }
+
+        return output;
+    }
+}
