107442a47c85d5ad064306cd17e6edd0cf219286
Ajith.j <ajith.j@qbrainx.com>
2024-02-05 18:17:17 +0530

    05/02/2024

--- src/LeetCode/FirstOccurrence.java
+++ src/LeetCode/FirstOccurrence.java
@@ -7,24 +7,27 @@ public class FirstOccurrence {
     public static void main(String[] args) {
         String haystack = "sadbutsad";
         String needle = "sad";
+        int length = haystack.length() - needle.length();
+
         List<String> list = new ArrayList<>();
 
-        occurrence(haystack, needle, list, 0, needle.length());
+        occurrence(haystack, needle, list, 0, needle.length(),length );
 
 
     }
 
-    static List occurrence(String haystack, String needle, List<String> list, int index, int length) {
+    static List occurrence(String haystack, String needle, List<String> list, int index, int length, int lastIndex) {
 
         StringBuilder stringBuilder = new StringBuilder();
-        if (index < haystack.length()) {
+
+        if (index <= lastIndex) {
             for (int i = index; i < length; i++) {
                 stringBuilder.append(haystack.charAt(i));
             }
             if (stringBuilder.toString().equals(needle)) {
                 list.add(stringBuilder.toString());
             }
-            occurrence(haystack, needle, list, index + 1, length + 1);
+            occurrence(haystack, needle, list, index + 1, length + 1, lastIndex);
         }
 
         return list;
