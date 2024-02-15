347cc8f7f28dc57b138c597692de0e4b90189dc3
Ajith <ajithashok2530@gmail.com>
2024-02-15 20:50:23 +0530

    15/02/24

--- src/HackerRank/FizzBuzz.java
+++ src/HackerRank/FizzBuzz.java
@@ -6,26 +6,16 @@ public class FizzBuzz {
         int n = 15;
 
         for (int i = 1; i <= n; i++) {
-
-            boolean value = true;
             if (i % 3 == 0 && i % 5 != 0) {
                 System.out.println("Fizz");
-                value = false;
-            }
-            if (i % 5 == 0 && i % 3 != 0) {
+            } else if(i % 5 == 0 && i % 3 != 0) {
                 System.out.println("Buzz");
-                value = false;
-            }
-            if (i % 3 == 0 && i % 5 == 0) {
+            } else if( i % 3 == 0 && i % 5 == 0) {
                 System.out.println("Fizz Buzz");
-                value = false;
-            }
-
-            if (value) {
+            } else  {
                 System.out.println(i);
             }
 
-            value = true;
         }
     }
 }
