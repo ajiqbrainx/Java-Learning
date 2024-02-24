9154c22e1af06d3652ef7f8aac122b909d773020
Ajith.j <ajith.j@qbrainx.com>
2024-02-24 17:33:00 +0530

    24/02/2024

--- src/Recusrion/Factorial.java
+++ src/Recusrion/Factorial.java
@@ -3,14 +3,14 @@ package Recusrion;
 public class Factorial {
     public static void main(String[] args) {
         int start = 1;
-        recursion(6, start);
+        recursion(5, start);
 
     }
 
     static void recursion(int number, int start) {
-        if (number != 1) {
-            start = start * number;
-            recursion(number - 1, start * number);
+        if (number > 1) {
+//            start = start * number;
+            recursion(number - 1, start * number );
         } else {
             System.out.println(start);
         }
