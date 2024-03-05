0889b045277a70e4a5c33cc39fc958e1feca2158
Ajith <ajithashok2530@gmail.com>
2024-03-05 21:40:32 +0530

    05/03/24

--- src/Java8Coding/Palindrome.java
+++ src/Java8Coding/Palindrome.java
@@ -1,13 +1,22 @@
 package Java8Coding;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Palindrome {
     public static void main(String[] args) {
 
-        String name = "madam";
+        String name = "madams";
+        System.out.println(name);
 
 
+     var out=   Arrays.stream(name.split("")).map(f->new StringBuffer(f).reverse()).toString();
+
+        System.out.println(out.toString());
+//       String output= Arrays.stream(name.split("")).map(f->new StringBuilder(f).reverse()).collect(Collectors.joining());
+//
+//
+//        System.out.println(output);
 
 
 
