f4817281fb74af297ddfd944228af347e6b98b39
Ajith.j <ajith.j@qbrainx.com>
2024-03-12 10:11:50 +0530

    12/03/2024

--- src/Java8Coding/FindElement.java
+++ src/Java8Coding/FindElement.java
@@ -6,9 +6,9 @@ import java.util.*;
 public class FindElement {
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(10, 15, 8, 49, 25, 98, 132);
+        List<Integer> list = Arrays.asList(-10, 15, 8, 49, 25, 98, 132);
 
-        list.stream().map(f -> f.toString()).filter(f -> f.startsWith("1")).forEach(System.out::println);
+        list.stream().map(f ->  f.toString()).filter(f -> f.startsWith("1")).forEach(System.out::println);
 
     }
 }
