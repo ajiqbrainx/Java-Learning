6b95e0f6ed71a4813f4c1865585903d960f35844
ajith <ajith@krestwolf.ai>
2025-01-28 10:17:26 +0530

    changes

--- src/Mine/Collection/Queue/PriorityQueueExample.java
+++ src/Mine/Collection/Queue/PriorityQueueExample.java
@@ -4,8 +4,12 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueExample {
     public static void main(String[] args) {
-        PriorityQueue<Integer>number=new PriorityQueue<>();
-        number.add(1);
+        PriorityQueue<Integer> number = new PriorityQueue<>();
+        number.add(3);
         number.add(2);
+        number.add(1);
+        System.out.println(number);
+        number.poll();
+        System.out.println(number);
     }
 }
--- src/Mine/Constructor/CopyConstructor.java
+++ src/Mine/Constructor/CopyConstructor.java
@@ -2,27 +2,30 @@ package Mine.Constructor;
 
 public class CopyConstructor {
 
-    int x,y;
-    CopyConstructor(int xx,int yy){
-        x=xx;
-        y=yy;
+    int x, y;
+
+    CopyConstructor(int xx, int yy) {
+        x = xx;
+        y = yy;
     }
 
-    CopyConstructor(CopyConstructor c){
+    CopyConstructor(CopyConstructor c) {
         System.out.println("Copy constructor is working fine....");
-        x=c.x;
-        y=c.y;
+        x = c.x;
+        y = c.y;
     }
-    void check(){
-       int z=x*y;
+
+    void check() {
+        int z = x * y;
         System.out.println(z);
     }
 }
-class Main2{
+
+class Main2 {
     public static void main(String[] args) {
-      CopyConstructor constructor=new CopyConstructor(22,33);
-      constructor.check();
-        CopyConstructor constructor1=new CopyConstructor(constructor);
+        CopyConstructor constructor = new CopyConstructor(22, 33);
+        constructor.check();
+        CopyConstructor constructor1 = new CopyConstructor(constructor);
         constructor1.check();
 
     }
