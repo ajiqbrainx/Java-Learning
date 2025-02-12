7e6ddfb072c9504d5f70b8594f5eb39fcfbd0228
Ajith <ajithashok2530@gmail.com>
2025-02-12 11:32:47 +0530

    palindrome

--- src/ETI/Practise/LongestPalindrome.java
+++ src/ETI/Practise/LongestPalindrome.java
@@ -1,7 +1,41 @@
 package ETI.Practise;
 
+import javax.print.DocFlavor;
+import java.util.LinkedList;
+import java.util.List;
+
 public class LongestPalindrome {
     public static void main(String[] args) {
 
+        String name = "asdabaaabber";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        List<String> list = new LinkedList<>();
+
+        for (int i = 0; i < name.length(); i++) {
+
+
+            for (int j = i; j < name.length(); j++) {
+
+                stringBuilder.append(name.charAt(j));
+                StringBuilder normal = new StringBuilder();
+                normal.append(stringBuilder);
+                if (normal.toString().contentEquals(normal.reverse())) {
+                    list.add(normal.toString());
+                }
+            }
+            stringBuilder.setLength(0);
+        }
+
+        System.out.println(list);
+        StringBuilder output=new StringBuilder();
+        for (String palindrome : list) {
+            if(palindrome.length() > output.length()){
+                output.setLength(0);
+                output.append(palindrome);
+            }
+        }
+
+        System.out.println(output);
     }
 }
