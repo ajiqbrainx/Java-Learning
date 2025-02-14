21795f2dcbcb7b7ac7d1ceee1370ae18ebb41cd3
Ajith <ajithashok2530@gmail.com>
2025-02-14 10:22:21 +0530

    changes added

--- src/Mine/AccessSpecifier/Checking.java
+++ src/Mine/AccessSpecifier/Checking.java
@@ -1,13 +1,13 @@
 package Mine.AccessSpecifier;
 
 public class Checking extends ProtectedEx {
-    void sub(){
-        int c=a-b;
+    void sub() {
+        int c = a - b;
         System.out.println(c);
     }
 
     public static void main(String[] args) {
-        Checking checking  =new Checking();
+        Checking checking = new Checking();
         checking.sub();
     }
 }
