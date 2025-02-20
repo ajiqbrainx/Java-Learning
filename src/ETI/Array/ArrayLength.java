91a93db9814e02bae0db6a83b1b8ecb67e995a78
Ajith <ajithashok2530@gmail.com>
2025-02-20 10:06:32 +0530

    ListStringLength find

--- src/ETI/Array/ArrayLength.java
+++ src/ETI/Array/ArrayLength.java
@@ -8,14 +8,17 @@ import java.util.List;
 public class ArrayLength {
     public static void main(String[] args) {
 
-        List<String> list = Arrays.asList("Hello", "to", "java", "world");
-        int output = 0;
-        for (String name : list) {
-//            output = name.length() > output ? name.length() : output;
-            output = Math.max(name.length(), output);
-        }
+        List<String> list = Arrays.asList("");
 
-
+       var output= list.stream().mapToInt(String::length).max().orElse(0);
         System.out.println(output);
+//
+//        int output = 0;
+//        for (String name : list) {
+//            output = name.length() > output ? name.length() : output;
+//        }
+//
+//
+//        System.out.println(output);
     }
 }
