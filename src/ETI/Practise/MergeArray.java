02082674a014a63689a01b80230edff3cb7479d1
Ajith <ajithashok2530@gmail.com>
2025-03-05 18:52:53 +0530

    Merge list

--- src/ETI/Practise/MergeArray.java
+++ src/ETI/Practise/MergeArray.java
@@ -1,7 +1,10 @@
 package ETI.Practise;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MergeArray {
     public static void main(String[] args) {
@@ -16,7 +19,14 @@ public class MergeArray {
         System.out.println();
         var mergeArray = IntStream.concat(Arrays.stream(array1), Arrays.stream(array2)).toArray();
 
+
         Arrays.stream(mergeArray).forEach(f -> System.out.print(f + " "));
+
+        System.out.println();
+        List<Integer> list1 = List.of(1, 2, 3, 4, 5);
+        List<Integer> list2 = List.of(10, 20, 30, 40, 50);
+        var mergeList = Stream.concat( list1.stream(), list2.stream()).collect(Collectors.toList());
+        System.out.println(mergeList);
 //        legacy way
 //        int[] mergeArray = new int[array1.length + array2.length];
 //        System.out.println();
