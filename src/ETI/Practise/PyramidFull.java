4ab2088ab49aab95c620e93689d892fb826346ec
Ajith <ajithashok2530@gmail.com>
2025-03-05 17:50:39 +0530

    pyramid changes

--- src/ETI/Practise/PyramidFull.java
+++ src/ETI/Practise/PyramidFull.java
@@ -10,7 +10,7 @@ public class PyramidFull {
         for (int i = 1; i <= rows; i++) {
 
             for (int j = i; j < rows; j++) {
-                System.out.print("-");
+                System.out.print(" ");
             }
 
             for (int k = 1; k <= (2 * i -1);k++){
